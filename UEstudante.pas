6fd7aef2dc87513ec51ff537c2b93ea0d14c1f44
Arthur Folly <arthurfollynf@gmail.com>
2025-08-08 16:04:09 -0400

    Procedures novas  com os codigos devidos, a de carregar a lista e limpar a lista, salvar o arquivo no formato UTF8, atualizarListas e cridoo clear do campos

--- UEstudante.pas
+++ UEstudante.pas
@@ -48,39 +48,98 @@ implementation
 
 procedure TFEstudantes.EnsureLista;
 begin
+    if FArquivo = '' then begin
+       FArquivo := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + 'estudantes.txt';
+      if FLista = nil then begin
+         FLista := TStringList.Create;
+      end;
+
+
+    end;
 
 end;
 
 function TFEstudantes.MontarLinha(const Cod: Integer; const Nome: string): string;
 begin
 
-  Result := '';
+  Result := IntToStr(Cod) + 'n' + Nome;
 end;
 
 procedure TFEstudantes.LerLinha(const Linha: string; out Cod: Integer; out Nome: string);
+var
+  p: integer;
 begin
+  p := Pos(';', Linha); // Encontra o delimitador de ponto e vírgula
+  if p > 0 then
+  begin
+    Cod := StrToIntDef(Copy(Linha, 1, p - 1), 0); // Obtém o código e converte para inteiro
+    Nome := Copy(Linha, p + 1, MaxInt); // Obtém o nome
+  end
+  else
+  begin
+    // Se nenhum ponto e vírgula for encontrado, define valores padrão
+    Cod := 0;
+    Nome := Linha; // ou '' se preferir
+  end;
 
-  Cod  := 0;
-  Nome := '';
 end;
 
+
+
 function TFEstudantes.IndexPorCodigo(const Cod: Integer): Integer;
+var i,C : Integer;
+    N:String;
 begin
   Result := -1;
+  for I := 0 to FLista.Count -1  do begin
+   // Pega a linha atual e separa o código ('C') e o nome ('N').
+    LerLinha(FLista[i],C,N);
+    // Verifica se o código que acabamos de ler ('C') é o que estamos procurando ('Cod').
+    if C = Cod then begin
+      Result := i;
+      exit;
+    end;
+
+  end;
+
 end;
 
 procedure TFEstudantes.Carregar;
 begin
+  EnsureLista;     // garante que FLista existe
+  FLista.Clear;    // limpa a lista atual
+  if FileExists(FArquivo) then
+  FLista.LoadFromFile(FArquivo, TEncoding.UTF8); // lê do disco
 
 end;
 
 procedure TFEstudantes.Salvar;
-begin
 
+begin
+  EnsureLista; // O papel que é garantir que a FLista Exista
+  FLista.SaveToFile(FArquivo,TEncoding .UTF8);
 end;
 
 procedure TFEstudantes.AtualizarListaUI;
+var   i, C: Integer;
+      N: string;
 begin
+  lbEstudantes.Items.BeginUpdate;
+  try
+    lbEstudantes.Clear;
+    for i := 0 to FLista.Count - 1 do
+    begin
+      //Separa a linha por codigo (C) e (N)
+      LerLinha(FLista[i], C, N);
+      //Adiciona o estudante  na lista visual, mostrando o código e o nome.
+      //A função Format cria um texto como "codigo - nome".
+      //Aqui, o '%d' é substituido pelo valor do codigo ('C'),
+      //e o '%s' é substituido pelo valor do nome ('N');
+      lbEstudantes.Items.Add(Format('%d - %s', [C,N]));
+
+    end;
+  finally
+    lbEstudantes.Items.EndUpdate;
 
 end;
 
